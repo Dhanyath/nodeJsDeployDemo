8c3a211790875999fad025996b32f93c2bd66d58
Dhanyath <110220359+Dhanyath@users.noreply.github.com>
2023-08-18 10:03:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 9000;
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+app.get('/welcome', (req, res) => {
+    res.send('Welcome Back');
+  });
+
+app.listen(port, () => console.log('Example app is listening'));
